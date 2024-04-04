a3547740243089317567cfc9976a5ba80f6b98dd
Wizzy <msvanadrichem@deloitte.nl>
2024-04-04 18:07:15 +0200

    19. added delete

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fb0b713415fd3d6100cee7c77dcbb35d752223c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5839affbd810ec87fe65ccec13ed32e17dcec6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
