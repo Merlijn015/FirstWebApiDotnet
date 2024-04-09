830fe93fcc23a980b569dff3b6442647d9f066fe
Wizzy <msvanadrichem@deloitte.nl>
2024-04-09 14:10:28 +0200

    44. Added Authority

---
+++ WebApplication1/Authority/AppRepository.cs
@@ -0,0 +1,26 @@
+namespace FirstWebApi.Authority
+{
+    public static class AppRepository
+    {
+        private static List<Application> _applications = new List<Application>()
+        {
+            new Application
+            {
+                ApplicationId = 1,
+                ApplicationName = "MVCWebApp",
+                ClientId = "974765d5-9567-4ff9-8d0f-426865bc3f16",
+                Secret = "4bb51cf9-3276-4302-8999-04e9752bc4b8"
+            }
+        };
+
+        public static bool Authenticate(string clientId, string secret)
+        {
+            return _applications.Any(x => x.ClientId == clientId && x.Secret == secret);
+        }
+
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(x => x.ClientId == clientId);
+        }
+    }
+}
---
+++ WebApplication1/Authority/Application.cs
@@ -0,0 +1,10 @@
+namespace FirstWebApi.Authority
+{
+    public class Application
+    {
+        public int ApplicationId{get; set;}
+        public string? ApplicationName{get; set;}
+        public string? ClientId{get; set;}
+        public string? Secret{get; set;}
+    }
+}
