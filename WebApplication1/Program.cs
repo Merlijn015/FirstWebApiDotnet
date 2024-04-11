aec2a2f09dc045fb458d771513607f52a474c3ec
Wizzy <msvanadrichem@deloitte.nl>
2024-04-11 11:48:07 +0200

    55.0 half implemented headers in swagger

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,9 +1,22 @@
+using FirstWebApi.Filters.OperationFilter;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=> 
+{
+    c.OperationFilter<AuthorizationHeaderOperationFilter>();
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme{
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header
+    });
+});
 
 builder.Services.AddControllers();
 
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a00c85764a873a2413118e5624f0a96a94d8713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53a3447814750664961a75983f313913c4f2d502")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
